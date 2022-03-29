a4210587b339213ec0721e8ba0a628fb3338fc4c
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-29 06:56:34 +0200

    Add project files.

---
+++ ConsoleApp37/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program zliczający ilość wystąpień znaku w tekście");
+            Console.WriteLine("Podaj tekst");
+            string tekst = Console.ReadLine();
+            int licznikZnakow = 0;
+            foreach (char litera in tekst)
+
+            {
+                if (litera == 'a')
+                {
+                    licznikZnakow++; 
+                }
+            }
+            Console.WriteLine("Litera a wystąpiła {0} razy w podanym ciągu znaków", licznikZnakow);
+        }
+    }
+}
